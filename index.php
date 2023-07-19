2c713284f26acd640b60a2ff2aed8a10047256c2
Xround2303 <Xround2303@gmail.com>
2023-07-19 10:00:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,172 +1,116 @@
 <?php
 
-$sizeList = [
-    "0" => "0",
-    "1" => "4px",
-    "2" => "8px",
-    "3" => "12px",
-    "4" => "16px",
-    "5" => "20px",
-    "6" => "24px",
-    "7" => "28px",
-    "8" => "32px",
-    "9" => "36px",
-    "10" => "40px",
-];
-$sizeList2 = [
-    "1" => "4px",
-    "2" => "8px",
-    "3" => "12px",
-    "4" => "16px",
-    "5" => "20px",
-    "6" => "24px",
-    "7" => "28px",
-    "8" => "32px",
-    "9" => "36px",
-    "10" => "40px",
+
+
+$medaList = [
+	"sm" => 600,
+	"md" => 800,
+	"lg" => 1200,
+	"xl" => 1440,
 ];
 
+$sizes = [
+    6,
+    7,
+    8,
+    9,
+    10,
+    15,
+    20,
+    25,
+    30,
+    35,
+    40,
+    45,
+    50,
+    55,
+    60,
+    70,
+    80,
+    90,
+    100,
+    110,
+    120,
+    130,
+    140,
+    150,
+    160,
+    170,
+    180,
+    190,
+    200
+];
 
-$breakpointList = [
-    "" => "0px",
-    "sm-" => "428px",
-    "md-" => "768px",
-    "lg-" => "1024px",
-    "xl-" => "1366px"
+$properties = [
+    "p" => function($size) {
+        return sprintf("padding: %s px;", $size);
+    },
+    "px" => function($size) {
+        return sprintf("padding-left: %s px; padding-right: %s px;", $size, $size);
+    },
+    "py" => function($size) {
+        return sprintf("padding-top: %s px; padding-bottom: %s px;", $size, $size);
+    },
+    "pr" => function($size) {
+        return sprintf("padding-right: %s px;", $size);
+    },
+    "pl" => function($size) {
+        return sprintf("padding-left: %s px;", $size);
+    },
+    "pt" => function($size) {
+        return sprintf("padding-top: %s px;", $size);
+    },
+    "pb" => function($size) {
+        return sprintf("padding-bottom: %s px;", $size);
+    },
+
+
+
+    "m" => function($size) {
+        return sprintf("margin: %s px;", $size);
+    },
+    "mx" => function($size) {
+        return sprintf("margin-left: %s px; margin-right: %s px;", $size, $size);
+    },
+    "my" => function($size) {
+        return sprintf("margin-top: %s px; margin-bottom: %s px;", $size, $size);
+    },
+    "mr" => function($size) {
+        return sprintf("margin-right: %s px;", $size);
+    },
+    "ml" => function($size) {
+        return sprintf("margin-left: %s px;", $size);
+    },
+    "mt" => function($size) {
+        return sprintf("margin-top: %s px;", $size);
+    },
+    "mb" => function($size) {
+        return sprintf("margin-bottom: %s px;", $size);
+    },
 ];
 
 
-foreach ($breakpointList as $breakpointKey => $breakpointSize) {
-    echo "@media (min-width: {$breakpointSize}) {";
 
-    foreach ($sizeList as $sizeIndex => $sizeValue) {
-        $a = <<<EON
-            .m-{$breakpointKey}{$sizeIndex} {
-                margin: {$sizeValue} !important;
-            }
-            
-            .mt-{$breakpointKey}{$sizeIndex},
-            .my-{$breakpointKey}{$sizeIndex} {
-                margin-top: {$sizeValue};
-            }
-            
-            .mr-{$breakpointKey}{$sizeIndex},
-            .mx-{$breakpointKey}{$sizeIndex} {
-                margin-right: {$sizeValue} !important;
-            }
-            
-            .mb-{$breakpointKey}{$sizeIndex},
-            .my-{$breakpointKey}{$sizeIndex} {
-                margin-bottom: {$sizeValue} !important;
-            }
-            
-            .ml-{$breakpointKey}{$sizeIndex},
-            .mx-{$breakpointKey}{$sizeIndex} {
-                margin-left: {$sizeValue} !important;
-            }
-        EON;
+foreach ($sizes as $size) {
+    foreach ($properties as $key => $property) {
+        echo sprintf(".%s-%s { %s }", $key, $size, $property($size));
+    }
+}
 
-        echo $a;
+function stringSelectorList($prefix, $properties, $sizes): string
+{
+
+    foreach ($sizes as $size) {
+        foreach ($properties as $key => $property) {
+            $rows[] = sprintf(".%s-%s-%s { %s }", $key, $prefix, $size, $property($size));
+        }
     }
 
-    foreach ($sizeList as $sizeIndex => $sizeValue) {
-        $a = <<<EON
-            .p-{$breakpointKey}{$sizeIndex} {
-                padding: {$sizeValue} !important;
-            }
-            
-            .pt-{$breakpointKey}{$sizeIndex},
-            .py-{$breakpointKey}{$sizeIndex} {
-                padding-top: {$sizeValue} !important;
-            }
-            
-            .pr-{$breakpointKey}{$sizeIndex},
-            .px-{$breakpointKey}{$sizeIndex} {
-                padding-right: {$sizeValue} !important;
-            }
-            
-            .pb-{$breakpointKey}{$sizeIndex},
-            .py-{$breakpointKey}{$sizeIndex} {
-                padding-bottom: {$sizeValue} !important;
-            }
-            
-            .pl-{$breakpointKey}{$sizeIndex},
-            .px-{$breakpointKey}{$sizeIndex} {
-                padding-left: {$sizeValue} !important;
-            }
-        EON;
-
-        echo $a;
-    }
-
-    foreach ($sizeList2 as $sizeIndex => $sizeValue) {
-        $a = <<<EON
-            .m-{$breakpointKey}n{$sizeIndex} {
-                margin: -{$sizeValue} !important;
-            }
-            
-            .mt-{$breakpointKey}n{$sizeIndex},
-            .my-{$breakpointKey}n{$sizeIndex} {
-                margin-top: -{$sizeValue} !important;
-            }
-            
-            .mr-{$breakpointKey}n{$sizeIndex},
-            .mx-{$breakpointKey}n{$sizeIndex} {
-                margin-right: -{$sizeValue} !important;
-            }
-            
-            .mb-{$breakpointKey}n{$sizeIndex},
-            .my-{$breakpointKey}n{$sizeIndex} {
-                margin-bottom: -{$sizeValue} !important;
-            }
-            
-            .ml-{$breakpointKey}n{$sizeIndex},
-            .mx-{$breakpointKey}n{$sizeIndex} {
-                margin-left: -{$sizeValue} !important;
-            }
-            EON;
-
-        echo $a;
-    }
-
-
-    echo <<<EON
-            .m-{$breakpointKey}auto {
-                margin: auto !important;
-            }
-            
-            .mt-{$breakpointKey}auto,
-            .my-{$breakpointKey}auto {
-                margin-top: auto !important;
-            }
-            
-            .mr-{$breakpointKey}auto,
-            .mx-{$breakpointKey}auto {
-                margin-right: auto !important;
-            }
-            
-            .mb-{$breakpointKey}auto,
-            .my-{$breakpointKey}auto {
-                margin-bottom: auto !important;
-            }
-            
-            .ml-{$breakpointKey}auto,
-            .mx-{$breakpointKey}auto {
-                margin-left: auto !important;
-            }
-            
-            .h-{$breakpointKey}100 {
-                height: 100%;
-            }
-            
-            .h-{$breakpointKey}auto {
-                height: auto;
-            }
-        EON;
-
-    echo "}";
+    return implode("", $rows ?? []);
 }
 
 
-?>
+foreach ($medaList as $prefix => $media) {
+    echo sprintf("@media (min-width: %s px){ %s }", $media, stringSelectorList($prefix, $properties, $sizes));
+}
 
